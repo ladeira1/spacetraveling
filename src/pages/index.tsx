0a11258b5e8d129c8a1c91c5774911f69891b02e
Joao Ladeira <joaoladeirag@gmail.com>
2021-03-29 20:17:54 -0300

    add home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+import { FiUser } from 'react-icons/fi'
+
 import { GetStaticProps } from 'next';
 
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
-import styles from './home.module.scss';
+import styles from './home.module.scss'
 
 interface Post {
   uid?: string;
@@ -25,8 +30,33 @@ interface HomeProps {
 }
 
 export default function Home() {
+  const temp = [{ id: 0 }, { id: 1 }, { id: 2 }]
+
   return (
     <>
+      <Head>
+        <title>Home | spacetraveling.</title>
+      </Head>
+      <main className={styles.container}>
+        <div className={styles.list}>
+          {temp.map(t => (
+            <Link href="/" key={t.id}>
+              <a>
+                <strong>Como utilizar react hooks</strong>
+                <p>alo alo alo alo alo alo alo alo alo alo alo alo alo alo alo alo alo alo</p>
+                <footer>
+                  <time>29 Mar 2021</time>
+                  <FiUser />
+                  <span>Joao Ladeira</span>
+                </footer>
+              </a>
+            </Link>
+          ))}
+        </div>
+        <footer className={styles.footer}>
+          <button type="button">Carregar mais posts</button>
+        </footer>
+      </main>
     </>
   )
 }
